075f243c9d54fa10165cdd40888aa470fe785340
Honore Hounwanou <mercuryseries@gmail.com>
2020-10-15 02:20:11 -0400

    Add proper page titles

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Hello City</title>
+        <title>@yield('title', 'Hello City')</title>
     </head>
     <body>
         @yield('content')
--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,5 +1,7 @@
 @extends('app')
 
+@section('title', 'About Us | Hello City')
+
 @section('content')
     <p>Built with &hearts; by LES TEACHERS DU NET.</p>
 
