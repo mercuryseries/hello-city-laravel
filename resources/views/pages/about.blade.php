f7d3866c2700809d2a59bb1f338f977a9e98be80
Honore Hounwanou <mercuryseries@gmail.com>
2020-10-15 03:25:09 -0400

    Add images

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -3,6 +3,8 @@
 @section('title', 'About Us | ' . config('app.name'))
 
 @section('content')
+    <img src="/images/tdn-logo.png" alt="TDN Logo">
+
     <p>Built with &hearts; by LES TEACHERS DU NET.</p>
 
     <p><a href="{{ route('home') }}">Revenir à la page d'accueil</a></p>
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,6 +1,8 @@
 @extends('app')
 
 @section('content')
+    <img src="/images/quebec-flag.png" alt="Quebec Flag">
+
     <h1>Hello from Quebec!</h1>
 
     <p>It's currently {{ date('h:i A') }}.</p>
