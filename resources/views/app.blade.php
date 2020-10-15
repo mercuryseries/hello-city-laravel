e46ef11283b8d449148b0b150e5a76aaca74ff66
Honore Hounwanou <mercuryseries@gmail.com>
2020-10-15 03:14:48 -0400

    Do not display footer about us link on about page

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -12,7 +12,10 @@
         <footer>
             <p>
                 &copy; Copyright {{ date('Y') }}
+
+                @if(! Route::is('about'))
                 &middot; <a href="{{ route('about') }}">About Us</a>
+                @endif
             </p>
         </footer>
     </body>
