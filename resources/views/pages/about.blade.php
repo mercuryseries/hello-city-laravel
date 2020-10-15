fa250a4c636f7be994b53daa2d5ad3c28038b41d
Honore Hounwanou <mercuryseries@gmail.com>
2020-10-15 02:42:21 -0400

    Make use of configuration to avoid duplication

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,6 +1,6 @@
 @extends('app')
 
-@section('title', 'About Us | Hello City')
+@section('title', 'About Us | ' . config('app.name'))
 
 @section('content')
     <p>Built with &hearts; by LES TEACHERS DU NET.</p>
