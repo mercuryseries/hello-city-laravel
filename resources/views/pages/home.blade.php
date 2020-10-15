6bee99ba9c5e0d6d33dc3bd90294242b72647d4c
Honore Hounwanou <mercuryseries@gmail.com>
2020-10-15 05:00:22 -0400

    Reformat homepage html

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,7 +3,9 @@
 @section('content')
     <img src="{{ asset('images/quebec-flag.png') }}" alt="Quebec Flag" class="mt-12 rounded shadow-md h-32">
 
-    <h1 class="mt-5 text-3xl sm:text-5xl font-semibold text-indigo-600">Hello from Quebec!</h1>
+    <h1 class="mt-5 text-3xl sm:text-5xl font-semibold text-indigo-600">
+        Hello from Quebec!
+    </h1>
 
     <p class="text-lg text-gray-800">It's currently {{ date('h:i A') }}.</p>
 @endsection
