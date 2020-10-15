ba96e8c6253a77d2c357a5192575c31e621f7478
Honore Hounwanou <mercuryseries@gmail.com>
2020-10-15 01:37:45 -0400

    Add about page and update links

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,12 +4,12 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Hello City</title>
+        <title>About Us | Hello City</title>
     </head>
     <body>
-        <h1>Hello from Quebec!</h1>
+        <p>Built with &hearts; by LES TEACHERS DU NET.</p>
 
-        <p>It's currently {{ date('h:i A') }}.</p>
+        <p><a href="/">Revenir à la page d'accueil</a></p>
 
         <footer>
             <p>&copy; Copyright {{ date('Y') }} &middot; <a href="/about-us">About Us</a></p>
